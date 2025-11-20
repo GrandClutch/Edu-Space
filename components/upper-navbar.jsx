ad155ebd5719651edad3c4bc3edde60f547cffde
Chhin Long <grandclutch168@gmail.com>
2025-11-20 14:11:33 +0700

    feat: added logout for upper-navbar

--- components/upper-navbar.jsx
+++ components/upper-navbar.jsx
@@ -1,11 +1,42 @@
 "use client";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
-import { Bell, LogOut, User } from "lucide-react-native";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Bell, LogOut } from "lucide-react-native";
+import { useState } from "react";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
 
 const UpperNavBar = () => {
+  const [isLoggingOut, setIsLoggingOut] = useState(false);
   const router = useRouter();
+  const handleLogout = () => {
+    try {
+      setIsLoggingOut(true);
+      Alert.alert(
+        "Logout",
+        "Are you sure you want to logout?",
+        [
+          {
+            text: "Cancel",
+            style: "cancel",
+          },
+          {
+            text: "Logout",
+            style: "destructive",
+            onPress: async () => {
+              await AsyncStorage.removeItem("idToken");
+              router.replace("/login");
+            },
+          },
+        ],
+        { cancelable: true }
+      );
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }finally{
+      setIsLoggingOut(false);
+    }
+  };
   return (
     <View className="bg-accent px-5 pt-4 pb-6 flex-row items-center justify-between">
       <View className="flex-row items-center">
@@ -26,7 +57,7 @@ const UpperNavBar = () => {
         {/* <TouchableOpacity className="w-10 h-10 bg-white bg-opacity-30 rounded-full items-center justify-center">
           <User color={"#10266F"} />
         </TouchableOpacity> */}
-        <TouchableOpacity className="w-10 h-10 bg-white bg-opacity-30 rounded-full items-center justify-center">
+        <TouchableOpacity className="w-10 h-10 bg-white bg-opacity-30 rounded-full items-center justify-center" onPress={handleLogout}>
           <LogOut color={"red"} />
         </TouchableOpacity>
       </View>
