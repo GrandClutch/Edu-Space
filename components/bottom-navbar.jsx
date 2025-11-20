1ee9ddabe2751083a1c842afac1027ac088f9227
Chhin Long <grandclutch168@gmail.com>
2025-11-20 15:24:41 +0700

    fix: use router.replace instead of router.push to prevent stacking screens

--- components/bottom-navbar.jsx
+++ components/bottom-navbar.jsx
@@ -4,22 +4,31 @@ import { Text, TouchableOpacity, View } from "react-native";
 
 const BottomNavBar = () => {
   const router = useRouter();
+
+  const handleRouteToHome = () => {
+    router.replace("/home");
+  };
+
+  const handleRouteToAssignments = () => {
+    router.replace("/assignments");
+  };
+
   return (
     <View className="bg-darkBg flex-row items-center justify-around py-4 border-t border-accent">
       <TouchableOpacity
         className="items-center py-2"
-        onPress={() => router.push("/home")}
+        onPress={handleRouteToHome}
       >
         {/* <Text className="text-secondary text-2xl mb-1">🏠</Text> */}
         <Home color={"white"} />
         <Text className="text-white text-xs font-semibold">Home</Text>
       </TouchableOpacity>
       <TouchableOpacity
-        onPress={() => router.push("/assignments")}
+        onPress={handleRouteToAssignments}
         className="items-center py-2"
       >
         {/* <Text className="text-white text-2xl mb-1">📝</Text> */}
-        <Book color={'white'}/>
+        <Book color={"white"} />
         <Text className="text-white text-xs">Assignments</Text>
       </TouchableOpacity>
     </View>
